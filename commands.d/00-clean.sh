b0d565d9c986865a5450482d9cb4e62b669ba986
Roy van de Water <roy.vwater@gmail.com>
2015-07-25 12:53:08 -0700

    Get it working

--- commands.d/00-clean.sh
+++ commands.d/00-clean.sh
@@ -2,9 +2,13 @@
 
 fleetctl list-unit-files -no-legend -fields unit | xargs fleetctl destroy
 
-echo "Murdering docker"
-for CONTAINER in "$(docker ps --quiet)"; do
-  docker kill $CONTAINER > /dev/null
-done
+CONTAINERS=$(docker ps --quiet)
+
+if [ -n "$CONTAINERS" ]; then
+  echo "Murdering docker"
+  for CONTAINER in "${CONTAINERS}"; do
+    docker kill $CONTAINER > /dev/null
+  done
+fi
 
 etcdctl rm --recursive /vulcand &> /dev/null
--- commands.d/10-update-etcd.sh
+++ commands.d/10-update-etcd.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-ETCD_PAIRS=$(cat $INSTALL_DIR/config.d/*)
+ETCD_PAIRS=$(cat $INSTALL_DIR/config.d/* 2> /dev/null)
 
-while read -r ETCD_PAIR; do
-  PAIR=($ETCD_PAIR)
-  KEY=${PAIR[0]}
-  VALUE=${PAIR[1]}
+if [ -n "$ETCD_PAIRS" ]; then
+  while read -r ETCD_PAIR; do
+    PAIR=($ETCD_PAIR)
+    KEY=${PAIR[0]}
+    VALUE=${PAIR[1]}
 
-  etcdctl set $KEY $VALUE > /dev/null
-done <<< "$ETCD_PAIRS"
+    etcdctl set $KEY $VALUE > /dev/null
+  done <<< "$ETCD_PAIRS"
+fi
