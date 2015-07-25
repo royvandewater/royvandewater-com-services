fd371356c16f996ca1d4405a9cee58d6951320fd
Roy van de Water <roy.vwater@gmail.com>
2015-07-25 12:41:33 -0700

    Shuffle the services

--- commands.d/00-clean.sh
+++ commands.d/00-clean.sh
@@ -2,14 +2,9 @@
 
 fleetctl list-unit-files -no-legend -fields unit | xargs fleetctl destroy
 
-echo "murdering docker"
+echo "Murdering docker"
 for CONTAINER in "$(docker ps --quiet)"; do
   docker kill $CONTAINER > /dev/null
 done
 
-
-
-etcdctl rm --recursive /octoblu &> /dev/null
-etcdctl rm --recursive /docker &> /dev/null
-etcdctl rm --recursive /meshblu &> /dev/null
 etcdctl rm --recursive /vulcand &> /dev/null
